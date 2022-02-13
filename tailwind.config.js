4a3f529920a9288b1316171b2a39d2656d85c30d
= <bbhpowpow@gmail.com>
2022-02-13 21:56:32 +0700

    test

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ['./*.html'],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
+};
